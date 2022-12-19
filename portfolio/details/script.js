1c6c1519b60c7f3820232cd69484af75b6eefdf2
Nils Schneider-Sturm <n@schnst.at>
2022-12-19 15:02:48 +0100

    added iphone

--- portfolio/details/script.js
+++ portfolio/details/script.js
@@ -3,25 +3,27 @@ let previewImages = document.querySelectorAll(".preview-images .image");
 previewImages.forEach(image => {
     image.addEventListener("click", e => {
         let boundingRect = image.getBoundingClientRect();
-        console.log(boundingRect)
-        anim_wrapper.appendChild(image);
-        let scale = 2;
-       /* const html = new mojs.Html({
-            el: image,
-            x: {[boundingRect.x + "px"]: (window.innerWidth / 2 - boundingRect.width / 2) + "px"},
-            y: {[boundingRect.y + "px"]: (window.innerHeight / 2 - boundingRect.height / 2) + "px"},
-            scale: {1: 2}
-        })*/
-        image.style.position = "absolute";
-        image.style.top = boundingRect.top + 28 + "px";
-        image.style.left = boundingRect.left + 32 + "px";
-        anim_wrapper.style.zIndex = "10";
-//        image.style.transform = "translateX(" + boundingRect.top + 28 + "px" +") translateY(" +boundingRect.left + 32 + "px" + ") scale(2)";
 
-  //      image.style.transform = "translateX(calc(50vh - 50%)) translateY(calc(50vw - 50%)) scale(2)";
-        setTimeout(() =>     image.classList.add("focused-big"), 200        )
-  anim_wrapper.style.backdropFilter = "blur(4px)";
-       // html.play()
+        if (! image.classList.contains("focused-big")) {
+            image.setAttribute("data-index", Array.from(previewImagesDiv.children).indexOf(image));
+            anim_wrapper.appendChild(image);
+            image.style.zIndex = "10";
+            image.style.position = "absolute";
+            image.style.top = boundingRect.top + "px";
+            image.style.left = boundingRect.left + "px";
+            anim_wrapper.classList.add("overlay")
+            setTimeout(() => image.classList.add("focused-big"), 10);
+        } else {
+            image.classList.remove("focused-big");
+            anim_wrapper.classList.remove("overlay");
+            setTimeout(() => {
+                console.log(Number(image.getAttribute("data-index")))
+                previewImagesDiv.insertBefore(image, previewImagesDiv.childNodes[Number(image.getAttribute("data-index"))]);
+                image.style.position = null;
+                image.style.top = null;
+                image.style.left = null;
+                image.style.zIndex = null;
+            }, 500);
+        }
     })
-    console.log(image)
 })
