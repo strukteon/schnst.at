f3b776c580d45961e588211b4fdf443d5e9ce09b
Nils <sensentod@gmail.com>
2022-12-30 19:33:10 +0100

    added basic mobile responsiveness

--- portfolio/details/script.js
+++ portfolio/details/script.js
@@ -6,13 +6,14 @@ imageOverlay.childNodes.forEach(c => c.addEventListener("click", e => e.stopProp
 let previewImagesClones = [];
 let visible_index = -1;
 let [btn_prev, btn_next] = document.querySelectorAll(".image-overlay button");
+document.body.style.setProperty("--window-width", String(window.innerWidth));
 
 previewImages.forEach(image => {
     let clone = image.cloneNode(true);
     imageOverlay.appendChild(clone);
     previewImagesClones.push(clone);
 
-    clone.style.setProperty("--device-scale", window.innerHeight * 0.9 / Number(clone.style.getPropertyValue("--device-height").split("px")[0]))
+    clone.style.setProperty("--device-scale", calc_clone_scale(clone))
     clone.addEventListener("click", e => e.stopPropagation())
 
     image.addEventListener("click", e => {
@@ -21,10 +22,9 @@ previewImages.forEach(image => {
 });
 
 addEventListener("resize", () => {
+    document.body.style.setProperty("--window-width", String(window.innerWidth));
     previewImagesClones.forEach(c => 
-        c.style.setProperty("--device-scale",
-          window.innerHeight * 0.9 / 
-          Number(c.style.getPropertyValue("--device-height") .split("px")[0])))
+        c.style.setProperty("--device-scale", calc_clone_scale(c)))
 });
 
 function open_image_clone(imgc) {
@@ -34,6 +34,16 @@ function open_image_clone(imgc) {
     visible_index = previewImagesClones.indexOf(imgc);
     btn_prev.disabled = visible_index == 0;
     btn_next.disabled = visible_index == previewImagesClones.length - 1;
+}
+function calc_clone_scale(clone) {
+    let [h, w] = [Number(clone.style.getPropertyValue("--device-height") .split("px")[0]), Number(clone.style.getPropertyValue("--device-width") .split("px")[0])];
+    let [h_max, w_max] = [0.9, 0.7];
+    let h_scale = window.innerHeight * h_max / h;
+    let w_scale = window.innerWidth * w_max /  w;
+    console.log({h_scale, w_scale, h_max, w_max})
+    if (h * w_scale > window.innerHeight * h_max) return h_scale;
+    return w_scale;
+
 }
 imageOverlay.addEventListener("click", () => {
     imageOverlay.classList.remove("visible");
